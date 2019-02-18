0cb5364b53bd2697784efe3dc35566ad5788a716
fcakyon <akyon@ee.bilkent.edu.tr>
2019-02-18 23:13:44 +0300

    added assetbundle creation script

---
+++ Assets/Scripts/Editor/ExportAssetBundles.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.iOS);
+    }
+
+//    [MenuItem("Assets/Clear Cache")]
+    static void ClearCashe()
+    {
+    	Caching.ClearCache();
+    }
+}
