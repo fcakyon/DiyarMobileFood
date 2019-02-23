b5d0d70a1dbe59f9b21c5993109421bf8b97456d
Omer Cem Akyol <omercema@gmail.com>
2019-02-23 17:42:08 +0300

    List init added

---
+++ Assets/Scripts/Requests/ModelListInit.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ModelListInit : MonoBehaviour {
+
+    public GameObject modelButtonPrefab;
+
+    [System.Serializable]
+    public class Model
+    {
+        public string name;
+        public string category;
+        public string prefabLink;
+        public string imageUrl;
+    }
+
+    [System.Serializable]
+    public class ModelsObjectArray
+    {
+        public Model[] models;
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(GetModelsAndCreateButtons());
+    }
+	
+	// Update is called once per frame
+	void Update () {}
+
+    IEnumerator GetModelsAndCreateButtons()
+    {
+        UnityWebRequest request = UnityWebRequest.Get("https://diyar-server.herokuapp.com/v0/models/");
+        yield return request.SendWebRequest();
+        ModelsObjectArray modelsObjectArray = JsonUtility.FromJson<ModelsObjectArray>("{\"models\":" + request.downloadHandler.text + "}");
+        foreach(Model model in modelsObjectArray.models)
+        {
+            GameObject modelButton = Instantiate(modelButtonPrefab);
+            //modelButton.GetComponent<FoodModelConnection>().assetBundleUrl = model.prefabLink;
+            UnityWebRequest imageRequest = UnityWebRequestTexture.GetTexture(model.imageUrl);
+            yield return imageRequest.SendWebRequest();
+            Transform content = modelButton.transform.Find("Content");
+            Text text = content.Find("Text").GetComponent<Text>();
+            text.text = model.name;
+            Image image = content.Find("Image").GetComponent<Image>();
+            Texture2D texture = DownloadHandlerTexture.GetContent(imageRequest);
+            image.sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
+            modelButton.name = model.name;
+            modelButton.SetActive(true);
+            modelButton.transform.SetParent(gameObject.transform);
+            modelButton.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
+
+
