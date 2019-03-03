f19e05601302e58c561c560b8cac0ceab4afa6bf
Omer Cem Akyol <omercema@gmail.com>
2019-03-03 21:28:16 +0300

    Url fixed in Api.cs

--- Assets/Scripts/Requests/Api.cs
+++ Assets/Scripts/Requests/Api.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class Api : MonoBehaviour {
 
-    //public static readonly string ModelUrl = "https://diyar-server.herokuapp.com/v0/models/";
-    public static readonly string ModelUrl = "localhost:3000/v0/models/";
+    public static readonly string ModelUrl = "https://diyar-server.herokuapp.com/v0/models/";
+    //public static readonly string ModelUrl = "localhost:3000/v0/models/";
 }
