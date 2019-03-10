abdd16c7991f5b0d237ef8ba6b5c70aff49ba6ba
Omer Cem Akyol <omercema@gmail.com>
2019-03-10 15:32:01 +0300

    gitignore update

---
+++ Assets/Scripts/Services/Ios/IosServices.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEditor.Callbacks;
+//using UnityEditor;
+//#if UNITY_IOS
+//using UnityEditor.iOS.Xcode;
+//#endif
+
+public class IosServices : MonoBehaviour {
+
+    //[PostProcessBuild(99)]
+    //private static void AddCapabilities(BuildTarget buildTarget, string pathToBuiltProject)
+    //{
+    //    if (buildTarget == BuildTarget.iOS)
+    //    {
+    //        string projPath = pathToBuiltProject + "/Unity-iPhone.xcodeproj/project.pbxproj";
+    //        string entitlementsFileName = "diyar.entitlements";
+    //        string targetName = "Unity-iPhone";
+    //        var entitlements = new ProjectCapabilityManager(projPath, entitlementsFileName, targetName);
+    //        entitlements.AddAssociatedDomains(new string[] { "applinks:myurl.com" });
+    //        entitlements.WriteToFile();
+    //    }
+    //}
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
