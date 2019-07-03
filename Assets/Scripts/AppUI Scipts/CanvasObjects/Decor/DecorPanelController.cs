03fa9d673aa41c74132f92bdcaceaff28558e74b
Omer Cem Akyol <omercema@gmail.com>
2019-07-03 14:44:14 +0300

    Panel Update

--- Assets/Scripts/AppUI Scipts/CanvasObjects/Decor/DecorPanelController.cs
+++ Assets/Scripts/AppUI Scipts/CanvasObjects/Decor/DecorPanelController.cs
@@ -6,6 +6,7 @@ public class DecorPanelController : MonoBehaviour {
 
     public GameObject decorPopupPanel;
     public GameObject decorInfoPanel;
+    public List<GameObject> gameObjectsToToggleWithPopulPanel = new List<GameObject>();
     private DecorPopupPanelController decorPopupPanelController;
     private DecorPopupPanelController decorInfoPanelController;
 
@@ -13,12 +14,14 @@ public class DecorPanelController : MonoBehaviour {
     {
         decorPopupPanelController = decorPopupPanel.GetComponent<DecorPopupPanelController>();
         decorPopupPanelController.ClosePanel();
+        gameObjectsToToggleWithPopulPanel.ForEach((GameObject obj) => obj.SetActive(true));
     }
 
     public void OpenPopupPanel()
     {
         decorPopupPanelController = decorPopupPanel.GetComponent<DecorPopupPanelController>();
         decorPopupPanelController.OpenPanel();
+        gameObjectsToToggleWithPopulPanel.ForEach((GameObject obj) => obj.SetActive(false));
     }
 
     public void CloseInfoPanel()
